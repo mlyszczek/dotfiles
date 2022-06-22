d5bec3063f7d1a280dea353eefb5e8536cbc62a1
Michał Łyszczek <michal.lyszczek@bofc.pl>
2022-06-22 10:39:12 +0200

    update .tmux/weechat-hotlist.sh

--- .tmux/weechat-hotlist.sh
+++ .tmux/weechat-hotlist.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
 . ${HOME}/.tmux/utils.sh
-
-if [ $(hostname) != marchewa ]; then
-	printf ""
-	exit 0
-fi
+run_only_on_hostname marchewa
+picto="📟"
 
 count=0
 if [ -r /tmp/hotlist-udp-current ]; then
 	count=$(cat /tmp/hotlist-udp-current)
-fi
-
-if [ "x${UNICODE_FONT}" = "x1" ]; then
-	picto="📟"
-else
-	picto="irc"
 fi
 
 if [ -z "${count}" ] || ! [ ${count} -eq ${count} ] || [ ${count} -eq 0 ]; then
