9daecb6a680c1b20f33a2d49bcab5faa36f7f95e
Michał Łyszczek <michal.lyszczek@bofc.pl>
2022-06-22 10:39:12 +0200

    update .tmux/alarm.sh

--- .tmux/alarm.sh
+++ .tmux/alarm.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
+. ${HOME}/.tmux/utils.sh
+run_only_on_hostname marchewa
+picto="⏰"
+
 left=$(alarm --show)
 if [ $? -eq 0 ]; then
 	count=$left
-fi
-
-if [ "x${UNICODE_FONT}" = "x1" ]; then
-	picto="⏰"
-else
-	picto="alarm"
 fi
 
 if [ -z "${count}" ]; then
