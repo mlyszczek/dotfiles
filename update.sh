fa084946e65db3f7ea086e302cb303cdbaab240e
Michał Łyszczek <michal.lyszczek@bofc.pl>
2024-09-25 22:34:19 +0200

    update update.sh

--- update.sh
+++ update.sh
@@ -4,11 +4,11 @@ cd $(dirname $0)
 git pull -q
 
 cd .powerlevel10k
-git checkout origin/master
+git checkout -q origin/master
 git pull -q origin master
 cd ..
 
 cd .zsh-autosuggestions
-git checkout origin/master
+git checkout -q origin/master
 git pull -q origin master
 cd ..
