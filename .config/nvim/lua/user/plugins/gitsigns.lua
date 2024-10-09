4c690e68bf7f7023cc497096d9fe4e0c9fd87240
Michał Łyszczek <michal.lyszczek@bofc.pl>
2024-10-09 09:10:24 +0200

    update .config/nvim/lua/user/plugins/gitsigns.lua

--- .config/nvim/lua/user/plugins/gitsigns.lua
+++ .config/nvim/lua/user/plugins/gitsigns.lua
@@ -2,11 +2,20 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
+			add          = { text = "+" },
+			change       = { text = "~" },
+			delete       = { text = "_" },
+			topdelete    = { text = "‾" },
 			changedelete = { text = "~" },
+			untracked    = { text = '┆' },
+		},
+		signs_staged = {
+			add          = { text = '┃' },
+			change       = { text = '┃' },
+			delete       = { text = '_' },
+			topdelete    = { text = '‾' },
+			changedelete = { text = '~' },
+			untracked    = { text = '┆' },
 		},
 	},
 }
