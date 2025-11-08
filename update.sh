1191c051a05667481cb1cc61be78a47eabe63867
Michał Łyszczek <michal.lyszczek@bofc.pl>
2025-11-08 09:01:26 +0100

    update update.sh

--- update.sh
+++ update.sh
@@ -2,17 +2,4 @@
 
 cd $(dirname $0)
 git pull -q
-
-for d in .powerlevel10k .zsh-autosuggestions; do
-	cd $d
-	git checkout -q origin/master
-	git pull -q origin master
-	cd ..
-done
-
-for d in .zinit; do
-	cd $d
-	git checkout -q origin/main
-	git pull -q origin main
-	cd ..
-done
+git submodule update --init --recursive
